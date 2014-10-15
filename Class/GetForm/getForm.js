0db30559944fa25643e4c749f3c59da021a55579
em2538269 <ermu2012@gmail.com>
2014-10-15 10:53:35 -0700

    Signed-off-by: em2538269 <ermu2012@gmail.com>

--- Class/GetForm/getForm.js
+++ Class/GetForm/getForm.js
@@ -3,7 +3,7 @@ function getForm(url){
 	var info=url.split("?");
 	var nameValuePairs=info[1].split("&");
 	var $_GET = new Object;
-	for (var i=0;i<nameValuePairs.length;i++){
+	for (var i=0;i<nameValuePairs.length-1;i++){
 		var map = nameValuePairs[i].split("=");
 		var name=map[0];
 		var value=map[1];
